fcdbd55c6dd1392634081e4f3a00cc0fba982009
Tom Kysar <tom@augur.net>
2017-06-27 13:51:45 -0700

    added youtube video modal back

---
+++ js/youtube.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $("#myModal").on("hidden.bs.modal",function(){
+    $("#iframeYoutube").attr("src","#");
+  })
+})
+
+function changeVideo(vId){
+  var iframe=document.getElementById("iframeYoutube");
+  iframe.src="https://www.youtube.com/embed/"+vId+"?autoplay=1";
+  $("#myModal").modal("show");
+}
