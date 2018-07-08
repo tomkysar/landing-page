537e022583b57291d24eed4d0ba1209fb76f5d09
Serena Randolph <serena.randolph@gmail.com>
2018-07-08 20:24:20 +0100

    Adding mobile nav.

---
+++ src/scripts/site.js
@@ -0,0 +1,16 @@
+// Show and Hide Mobile Nav
+
+var headerNav = document.getElementsByClassName('Header__nav')[0];
+var mobileBars = document.getElementsByClassName('Header__nav-mobile-open')[0];
+var mobileTimes = document.getElementsByClassName('Header__nav-mobile-close')[0];
+
+function showMobileNav() {
+	headerNav.classList.add('nav--open');
+}
+
+function hideMobileNav() {
+	headerNav.classList.remove('nav--open');
+}
+
+mobileBars.onclick = showMobileNav;
+mobileTimes.onclick = hideMobileNav;
